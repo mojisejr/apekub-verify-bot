be3dc330bb5784ca632ee31ad99190f5d9877541
PBSmoke <kittinamkorn@gmail.com>
2021-08-31 01:06:16 +0700

    fix express from BKC to BSC and burn VON

--- index.js
+++ index.js
@@ -1,111 +1,127 @@
 import ethers from 'ethers';
 import express from 'express';
 import chalk from 'chalk';
+import Sequelize from 'sequelize';
 
 const app = express();
 
-// TUK-KKUB LP on Vonder: 0xFB74CAc28EE2635F08C22800CCFD5200cDe52DDF
-const WETH = 'KKUB';
-const ERC20 = 'TUK';
+const sequelize = new Sequelize('postgres://postgres:syspass@localhost:5432/vonder');
+
+sequelize.define('transaction', {
+})
+
+const PORT = 5000;
+
 const data = {
-  WETH: '0x67eBD850304c70d983B2d1b93ea79c7CD6c3F6b5', //wbnb 
-  // to_PURCHASE: '0x19dade57B0BBCE7D5E859ba02846820f5c0c2b09',  // VonderToken
-  to_PURCHASE: '0xAAD64d9b17f86b3ba803369b0d59392b3744ab13', // TukToken 
-  factory: '0x447DdE468Fb3B185d395D8D43D82D6636d69d481',  //Vonder V2 factory
-  router: '0x54D851C39fE28b2E24e354B5E8c0f09EfC65B51A', //Vonder V2 router
-  recipient: '0x4D27D0c531Bd634D5A26f219596b84f171002FB1', //THE Deployer,
-  AMOUNT_OF_WBNB : '0.02',
-  Slippage : '3', //in Percentage
-  gasPrice : '50', //in gwei
+  express: {
+    BKC: '0xafe60DBC58659F22c884B463Aefa43d84c229152', // Mainnet
+    BSC: '0xE16f1b620123eF93B4Bb9B4fe5903E90BCb30588', // Testnet
+  },
+  AMOUNT_OF_WBNB: '0.02',
+  Slippage: '3', //in Percentage
+  gasPrice: '50', //in gwei
   // gasLimit : '345684' //at least 21000
   gasLimit: '200000'
 }
 
-let initialLiquidityDetected = false;
+const options = { gasPrice: 10e18, gasLimit: 5500000};
 
-const bkcMainnetUrl = 'https://rpc.bitkubchain.io'
-// const mnemonic = '';
-const privateKey = '778c8ab3ef982c03b7a972dc3f63293f20fb61991e7bcbf53ef3aea70763a50c';
-const provider = new ethers.providers.JsonRpcProvider(bkcMainnetUrl)
-// const wallet = ethers.Wallet.fromMnemonic(mnemonic);
-const wallet = new ethers.Wallet(privateKey);
-const account = wallet.connect(provider);
+// BKC
+const BKCMainnetUrl = 'https://rpc.bitkubchain.io'
+const BKCPrivateKey = '280e49399d195e0a01b61d55210baa56020d7f0341c1ff752ea596a2d9732eec';
+const BKCProvider = new ethers.providers.JsonRpcProvider(BKCMainnetUrl)
+const BKCWallet = new ethers.Wallet(BKCPrivateKey);
+const BKCAccount = BKCWallet.connect(BKCProvider);
 
-const factory = new ethers.Contract(
-  data.factory,
-  ['function getPair(address tokenA, address tokenB) external view returns (address pair)'],
-  account
-);
+// BSC
+const BSCMainnetUrl = 'https://data-seed-prebsc-1-s2.binance.org:8545'
+const BSCPrivateKey = '280e49399d195e0a01b61d55210baa56020d7f0341c1ff752ea596a2d9732eec';
+const BSCProvider = new ethers.providers.JsonRpcProvider(BSCMainnetUrl)
+const BSCWallet = new ethers.Wallet(BSCPrivateKey);
+const BSCAccount = BSCWallet.connect(BSCProvider);
 
-const router = new ethers.Contract(
-  data.router,
+const BKCexpressContracts = new ethers.Contract(
+  data.express.BKC,
   [
-    'function getAmountsOut(uint amountIn, address[] memory path) public view returns (uint[] memory amounts)',
-    'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)'
+    'function withdraw(address _receiver, uint256 _amount)',
+    'function burn(uint256 _amountBurn)',
+    'event Deposit(address, address, uint256);',
+    'event Withdraw(address, address, uint256);'
   ],
-  account
+  BKCAccount
 );
+
+const BSCexpressContracts = new ethers.Contract(
+  data.express.BSC,
+  [
+    'function withdraw(address _receiver, uint256 _amount)' ,
+    'function burn(uint256 _amountBurn)',
+    'event Deposit(address, address, uint256);',
+    'event Withdraw(address, address, uint256);'
+  ],
+  BSCAccount
+);
+
 
 const run = async () => {
-	console.log("Start running bot...")
-  const tokenIn = data.WETH;
-  const tokenOut = data.to_PURCHASE;
-  const pairAddress = await factory.getPair(tokenIn, tokenOut);
+  console.log("Start running bot...")
 
-  console.log("pairAddress:", pairAddress);
-  const pair = new ethers.Contract(pairAddress, ['event Mint(address indexed sender, uint amount0, uint amount1)'], account);
+  // try {
+  //   await sequelize.authenticate();
+  //   console.log('Connection has been established successfully.');
+  // } catch (error) {
+  //   console.error('Unable to connect to the database:', error);
+  // }
 
-  pair.on('Mint', async (sender, amount0, amount1) => {
-    if(initialLiquidityDetected === true) {
-        return;
-    }
+  //Ex
+  //We buy x amount of the new token for our wbnb
+  // const amountIn = ethers.utils.parseUnits(`${data.AMOUNT_OF_WBNB}`, 'ether');
+  // const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
 
-    initialLiquidityDetected = true;
+  // hook deposit
+  BKCexpressContracts.on('Deposit', async (tx, sender, amount) => {
+    console.log(this);
+    console.log('Processing Transaction.....');
+    console.log(chalk.yellow(`tx: ${tx}`));
+    console.log(chalk.yellow(`sender: ${sender}`));
+    console.log(chalk.yellow(`amountIn: ${amount}`));
 
-   //We buy x amount of the new token for our wbnb
-   const amountIn = ethers.utils.parseUnits(`${data.AMOUNT_OF_WBNB}`, 'ether');
-   const amounts = await router.getAmountsOut(amountIn, [tokenIn, tokenOut]);
- 
-   //Our execution price will be a bit different, we need some flexbility
-   const amountOutMin = amounts[1].sub(amounts[1].div(`${data.Slippage}`)); 
- 
-   console.log(
-    chalk.green.inverse(`Liquidity Addition Detected\n`)
-     +
-     `Buying Token
-     =================
-     tokenIn: ${amountIn.toString()} ${tokenIn} (${WETH})
-     tokenOut: ${amountOutMin.toString()} ${tokenOut} (${ERC20})
-   `);
+    // Call Withdraw
+    withdrawXVon(sender, amount)
+  });
 
-   console.log('Processing Transaction.....');
-   console.log(chalk.yellow(`amountIn: ${amountIn}`));
-   console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`));
-   console.log(chalk.yellow(`tokenIn: ${tokenIn}`));
-   console.log(chalk.yellow(`tokenOut: ${tokenOut}`));
-   console.log(chalk.yellow(`data.recipient: ${data.recipient}`));
-   console.log(chalk.yellow(`data.gasLimit: ${data.gasLimit}`));
-   console.log(chalk.yellow(`data.gasPrice: ${ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')}`));
+  BSCexpressContracts.on('Withdraw', async (tx, sender, amount) => {
+    console.log('Processing Transaction.....');
+    console.log(chalk.yellow(`tx: ${tx}`));
+    console.log(chalk.yellow(`sender: ${sender}`));
+    console.log(chalk.yellow(`amountIn: ${amount}`));
 
-   const tx = await router.swapExactTokensForTokens(
-     amountIn,
-     amountOutMin,
-     [tokenIn, tokenOut],
-     data.recipient,
-     Date.now() + 1000 * 60 * 10, //10 minutes
-     {
-       'gasLimit': data.gasLimit,
-       'gasPrice': ethers.utils.parseUnits(`${data.gasPrice}`, 'gwei')
-   });
- 
-   const receipt = await tx.wait(); 
-   console.log('Transaction receipt');
-   console.log(receipt);
+    // Call Withdraw
+    burnVon(sender, amount)
   });
 }
 
 run();
 
-const PORT = 5000;
+app.listen(PORT, (console.log(chalk.yellow("Start Service"))));
 
-app.listen(PORT, (console.log(chalk.yellow(`Listening for Liquidity Addition to token ${data.to_PURCHASE}`))));
+// function call 
+const withdrawXVon = async (address, amountOut) => {
+  console.log(chalk.yellow("withdrawXVon"));
+  console.log(chalk.yellow(`address : ${address}`));
+  console.log(chalk.yellow(`amountOut : ${amountOut}`));
+  const txWithdraw = await BSCexpressContracts.withdraw(address, amountOut.toString());
+  console.log(chalk.yellow(`tx : ${txWithdraw}`))
+}
+
+const burnVon = async (address, amountOut) => {
+  console.log(chalk.red("burnVon"));
+  console.log(chalk.red(`address : ${address}`));
+  console.log(chalk.red(`amountOut : ${amountOut}`));
+  const txBurn = await BKCexpressContracts.burn(amountOut.toString());
+  console.log(chalk.red(`tx : ${txBurn}`));
+}
+
+// function save to DB
+// save 
+
